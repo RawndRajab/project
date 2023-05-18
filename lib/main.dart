a9a31d116f292e44c013f6dec4e90c849721fc21
RawndRajab <rajabrawnd@gmail.com>
2023-05-18 16:28:14 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('hi'),
+        ),
+        body: Center(child: const Text('welcome')),
+      ),
+    ),
+  );
+}
